8dc61ff26d06b52074a4514d08e820104cda1b9c
Anthony <anth06ny@gmail.com>
2025-03-11 15:47:07 +0100

    ViewModel init correctement

--- composeApp/src/commonMain/kotlin/org/example/project/ui/Navigation.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/ui/Navigation.kt
@@ -30,7 +30,7 @@ fun AppNavigation(modifier: Modifier = Modifier) {
     val navHostController : NavHostController = rememberNavController()
     //viewModel() en dehors de NavHost lie à l'Activité donc partagé entre les écrans
     //viewModel() dans le NavHost lié à la stack d'écran. Une instance par stack d'écran
-    val mainViewModel : MainViewModel = viewModel()
+    val mainViewModel : MainViewModel = viewModel { MainViewModel()}
 
     //Import version avec Composable
     NavHost(navController = navHostController, startDestination = Routes.PhotographersRoute,
